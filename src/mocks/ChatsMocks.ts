cef97dd62be6b1142cfda33fdacd77cada2b2bca
kefflen <kefflenjava@hotmail.com>
2024-06-07 09:09:19 -0300

    chore: comment mocks that are imcompatible with its models

--- src/mocks/ChatsMocks.ts
+++ src/mocks/ChatsMocks.ts
@@ -1,66 +1,66 @@
 import { Chat } from "@/types/Chat"
 
 export const chatsMocks: Chat[] = [
-  {
-    id: '1',
-    title: 'Chat 1',
-    language: 'en',
-    context: 'Context chat 1',
-    summary: 'Summary chat 1',
-    messages: [
-      {
-        id: '1',
-        content: 'Introduction',
-        author: 'AIGO',
-        sentWhen: new Date()
-      },
-      {
-        id: '1',
-        content: 'Message 1',
-        author: 'Author 1',
-        sentWhen: new Date()
-      },
-      {
-        id: '1',
-        content: 'Message 2',
-        author: 'AIGO',
-        sentWhen: new Date()
-      },
-      {
-        id: '1',
-        content: 'Message 3',
-        author: 'Author 1',
-        sentWhen: new Date()
-      },
-      {
-        id: '1',
-        content: 'Message 4',
-        author: 'AIGO',
-        sentWhen: new Date()
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Chat 2',
-    language: 'en',
-    context: 'Context chat 2',
-    summary: 'Summary chat 2',
-    messages: [
-      {
-        id: '2',
-        content: 'Message 2',
-        author: 'Author 2',
-        sentWhen: new Date()
-      }
-    ]
-  },
-  {
-    id: '3',
-    title: 'Chat 3',
-    language: 'en',
-    context: 'Context chat 3',
-    summary: 'Summary chat 3',
-    messages: []
-  }
+  // {
+  //   id: '1',
+  //   title: 'Chat 1',
+  //   language: 'en',
+  //   context: 'Context chat 1',
+  //   summary: 'Summary chat 1',
+  //   messages: [
+  //     {
+  //       id: '1',
+  //       content: 'Introduction',
+  //       author: 'AIGO',
+  //       sentWhen: new Date()
+  //     },
+  //     {
+  //       id: '1',
+  //       content: 'Message 1',
+  //       author: 'Author 1',
+  //       sentWhen: new Date()
+  //     },
+  //     {
+  //       id: '1',
+  //       content: 'Message 2',
+  //       author: 'AIGO',
+  //       sentWhen: new Date()
+  //     },
+  //     {
+  //       id: '1',
+  //       content: 'Message 3',
+  //       author: 'Author 1',
+  //       sentWhen: new Date()
+  //     },
+  //     {
+  //       id: '1',
+  //       content: 'Message 4',
+  //       author: 'AIGO',
+  //       sentWhen: new Date()
+  //     }
+  //   ]
+  // },
+  // {
+  //   id: '2',
+  //   title: 'Chat 2',
+  //   language: 'en',
+  //   context: 'Context chat 2',
+  //   summary: 'Summary chat 2',
+  //   messages: [
+  //     {
+  //       id: '2',
+  //       content: 'Message 2',
+  //       author: 'Author 2',
+  //       sentWhen: new Date()
+  //     }
+  //   ]
+  // },
+  // {
+  //   id: '3',
+  //   title: 'Chat 3',
+  //   language: 'en',
+  //   context: 'Context chat 3',
+  //   summary: 'Summary chat 3',
+  //   messages: []
+  // }
 ]
